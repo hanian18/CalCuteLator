5d06d765ce75bc5ae78061344b8c52fa7046e999
Ian Roy Barcelona <ianroystudies@gmail.com>
2023-12-30 12:43:44 +0800

    This is the current situation of the calculator.
    Some function are not yet working. But I will update it as soon as possible,
    
    1. Only two items can be operated
    2. Backspace is not yet working
    3. Other than the basic operation... nothing is functional yet.
    4. Should be able to use the result(equal value) as first number and perform an operation.

--- src/main/java/com/example/javacalculator/HelloController.java
+++ src/main/java/com/example/javacalculator/HelloController.java
@@ -1,101 +1,112 @@
 package com.example.javacalculator;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 
 public class HelloController {
+    StringBuilder historyString = new StringBuilder();
+
+    private int equalClick = 0;
+    private int decimalClick = 0;
+    private String generalOperationObject;
+    @FXML
+    private TextField historyField;
+    @FXML
+    private TextField textField;
+    private double firstDouble;
 
     @FXML
-    private Button addButton;
+    void decimalHandlerAction(ActionEvent event) {
+        if(decimalClick==0){
+            String decimalObject = ((Button)event.getSource()).getText();
+            String oldText = textField.getText();
+            String newText = oldText + decimalObject;
+            textField.setText(newText);
+            historyString.append(newText);
+            historyField.setText(historyString.toString());
+            decimalClick++;
+        }
+    }
 
     @FXML
-    private Button backspaceButton;
+    void digitActionHandler(ActionEvent event) {
+        String digitObject = ((Button)event.getSource()).getText();
+        if(equalClick>0){
+            textField.setText("");
+            historyField.setText("");
+            historyString = new StringBuilder();
+        }
+        String oldText = textField.getText();
+        String newText = oldText + digitObject;
+        textField.setText(newText);
+        equalClick=0;
+    }
 
     @FXML
-    private Button clrButton;
+    void equalActionHandler(ActionEvent event) {
+        equalClick++;
+        double secondDouble;
+        double result=0;
+        String secondText = textField.getText();
+        secondDouble = Double.parseDouble(secondText);
+        String toAppend = textField.getText();
+        historyString.append(toAppend);
+        historyField.setText(historyString.toString());
+
+        switch(generalOperationObject){
+            case "+":
+                result = firstDouble+secondDouble;
+                break;
+            case "-":
+                result = firstDouble - secondDouble;
+                break;
+            case "x":
+                result = firstDouble * secondDouble;
+                break;
+            case "÷":
+                result = firstDouble / secondDouble;
+                break;
+
+            default:
+        }
+        String format = String.format("%.1f",result);
+        textField.setText(format);
+    }
 
     @FXML
-    private Button cosOperation;
+    void generalActionHandler(ActionEvent event) {
+        generalOperationObject = ((Button)event.getSource()).getText();
+        switch(generalOperationObject){
+            case "C":
+                decimalClick = 0;
+                textField.setText("");
+                historyField.setText("");
+                historyString = new StringBuilder();
+                equalClick = 0;
+                break;
+            case "+":
+            case "-":
+            case "x":
+            case "÷":
+            case "sin":
+            case "cos":
+            case "tan":
 
-    @FXML
-    private Button decimalPoint;
+                String oldText = textField.getText();
+                String newText = oldText + generalOperationObject;
+                String currentText = textField.getText();
+                firstDouble = Double.parseDouble(currentText);
+                textField.setText("");
+                historyString.append(newText);
+                historyField.setText(historyString.toString());
+                decimalClick=0;
+                break;
+                default:
 
-    @FXML
-    private Button divideButton;
-
-    @FXML
-    private Button eightButton;
-
-    @FXML
-    private Button equalButton;
-
-    @FXML
-    private Button exponentButton;
-
-    @FXML
-    private Button factorialFunction;
-
-    @FXML
-    private Button fiveButton;
-
-    @FXML
-    private Button fourButton;
-
-    @FXML
-    private Button leftParen;
-
-    @FXML
-    private Button logButton;
-
-    @FXML
-    private Button multiplyButton;
-
-    @FXML
-    private Button nineButton;
-
-    @FXML
-    private Button onOffButton;
-
-    @FXML
-    private Button oneButton;
-
-    @FXML
-    private Button percentButton;
-
-    @FXML
-    private Button piOperation;
-
-    @FXML
-    private Button rightParen;
-
-    @FXML
-    private Button sevenButton;
-
-    @FXML
-    private Button sinUperation;
-
-    @FXML
-    private Button sixButton;
-
-    @FXML
-    private Button sqrButton;
-
-    @FXML
-    private Button subtractButton;
-
-    @FXML
-    private Button tanOperation;
-
-    @FXML
-    private Button threeButton;
-
-    @FXML
-    private Button twoButton;
-
-    @FXML
-    private Button zeroButton;
-
-    //todo make the textfield getinput from the button, use setText
-    //todo crete a function of each operation
+        }
+    }
 
 }
